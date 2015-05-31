5110c14ab9796fbd8870c5e5587465d7b2dbc134
BJ Tiemessen <bj.tiemessen@gmail.com>
2015-05-31 01:42:26 -0500

    First Commit

---
+++ wp-random-image-widget.php
@@ -0,0 +1,24 @@
+<?php
+/******************************************************************************
+* Plugin Name: WP Random Image Widget
+* Plugin URI: https://www.bjtiemessen.com
+* Description: An image widget that displays a random image on each page load.
+* Version: 0.0.1
+* Author: BJ Tiemessen
+* Author URI: http://www.bjtiemessen.com
+* License: GPL-2.0+
+* License URI: http://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain: wp-random-image-widget
+******************************************************************************/
+
+//Define the plugin directory path
+if(!defined('WPRIW_DIR'))
+{
+	define('WPRIW_DIR', plugin_dir_path(__FILE__));
+}
+
+//Include functions and libraries
+require_once(WPRIW_DIR, 'includes/class-wpriw-widget.php');
+
+$wpriw = new WP_Random_Image_Widget();
+add_action('plugins_loaded', array($wpriw, 'init'));
